7b435a9f86c8d278532f46ee3966592b29f68875
sbayreder <sbayreder@gmail.com>
2020-04-24 22:13:38 -0500

    all html working properly

--- app/routing/server.js
+++ app/routing/server.js
@@ -1,2 +1,20 @@
 var express = require("express");
 var path = require("path");
+var friends = require("/app/data/friends.js");
+var app = express();
+require("dotenv").config();
+
+const apiRoutes = require('./app/routing/apiRoutes');
+const htmlRoutes = require('./app/routing/htmlRoutes');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('app/public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
